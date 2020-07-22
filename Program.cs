137da6ecae26d135f0a265767e6ef6d46da0c871
Tomas <kvedaravicius2@gmail.com>
2020-07-22 22:29:12 +0300

    json added

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using System.Configuration;
 
 namespace AnagramSolver
 {
@@ -6,7 +9,15 @@ namespace AnagramSolver
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"The parameters are: {GetParameters()}");
+        }
+        private static string GetParameters() 
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile(ConfigurationManager.AppSettings["Path"], optional: true, reloadOnChange: true);
+            var val1 = builder.Build().GetSection("Settings").GetSection("NumberOfAnagramsGenerated").Value;
+            var val2 = builder.Build().GetSection("Settings").GetSection("MinimumLengthOfInput").Value;
+
+            return $"The values of parameters are: {val1} and {val2}";
         }
     }
 }
