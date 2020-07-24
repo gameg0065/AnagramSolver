40c0de2680fa67fff1dde82bc639a5ca6bdd7898
Tomas <kvedaravicius2@gmail.com>
2020-07-24 09:30:21 +0300

    wip

--- AnagramSolver/Program.cs
+++ AnagramSolver/Program.cs
@@ -11,16 +11,12 @@ namespace AnagramSolver
     class Program
     {
         static void Main(string[] args) {
-            DictionaryManager.LoadDictionary(ConfigurationManager.AppSettings["DictionaryPath"]);
-            AnagramGenerator.GenerateAnagrams(GetUserInput.GetName());
-        }
-        private static string GetParameters()
-        {
             var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile(ConfigurationManager.AppSettings["Path"], optional: true, reloadOnChange: true);
-            var val1 = builder.Build().GetSection("Settings").GetSection("NumberOfAnagramsGenerated").Value;
-            var val2 = builder.Build().GetSection("Settings").GetSection("MinimumLengthOfInput").Value;
-
-            return $"The values of parameters are: {val1} and {val2}";
+            DictionaryManager.LoadDictionary(ConfigurationManager.AppSettings["DictionaryPath"]);
+            int inputLength = Int32.Parse(builder.Build().GetSection("Settings").GetSection("MinimumLengthOfInput").Value);
+            int anagramNumber = Int32.Parse(builder.Build().GetSection("Settings").GetSection("NumberOfAnagramsGenerated").Value);
+            string userInput = GetUserInput.GetWord(inputLength);
+            Console.WriteLine(AnagramGenerator.GenerateAnagrams(userInput, anagramNumber));
         }
     }
 }
