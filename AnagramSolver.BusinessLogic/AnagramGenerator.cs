6d08e891589da5237d9351255c65569e0e300065
Tomas <kvedaravicius2@gmail.com>
2020-07-31 08:16:08 +0300

    not working

--- AnagramSolver.BusinessLogic/AnagramGenerator.cs
+++ AnagramSolver.BusinessLogic/AnagramGenerator.cs
@@ -9,12 +9,21 @@ namespace AnagramSolver.BusinessLogic
     {
         public Dictionary<string, string> GenerateAnagrams(string word, int maxNumberOfAnagrams)
         {
+            Console.WriteLine();
             Dictionary<string, string> generatedAnagrams = new Dictionary<string, string>();
-            var test = word.ToCharArray();
             var q = word.Select(x => x.ToString());
+            var test = word.ToCharArray();
             for (int i = 0; i < 1; i++) {
-                List<string> mytest = new List<string>();
-                Recursion(test[i], i + 1, test, 0, mytest);
+                Recursion(test, word.Length - 1);
+                // List<char[]> test3 = new List<char[]>();
+                // test3.Add(ShiftToRight(test));
+                // test3.ForEach(delegate (char[] name)
+                // {
+                //     Print(name);
+                //     Recursion(word, word.Length - 1);
+                // });
+                Console.WriteLine(test);
+
             }
             foreach (var item in q) {
                 // Console.WriteLine(item);
@@ -25,17 +34,50 @@ namespace AnagramSolver.BusinessLogic
             }
             return generatedAnagrams;
         }
-        public void Recursion(char letter, int number, char[] word, int max, List<string> newWord) {
-            max++;
-            if(number < 0 || number > word.Length - 1 || max == word.Length) {
-                Console.WriteLine("ESCAPED from if "  + letter);
-            } else {
-                for (var m = number; m < word.Length; m++) {
-                    Console.WriteLine("ESCAPED " + letter);
-                    Recursion(word[m], word.Length - m, word, max, newWord);
+        void Print(char[] s)
+        {
+            string test = new string(s);
+            Console.WriteLine(test);
+        }
+        public void Recursion(char[] word, int max) {
+            if(max - 1 >= 0) {
+                max--;
+                char temp = word[max];
+                word[max] = word[max + 1];
+                word[max + 1] = temp;
+                string test1 = new string(word);
+                for (int y = 0; y < 2; y++)
+                {
+                    List<char[]> test3 = new List<char[]>();
+                    test3.Add(Switch(word));
+                    test3.ForEach(Print);
+                    test3.ForEach(delegate (char[] name) {
+                        Print(name);
+                        Recursion(name, max);
+                    });
+                    // string test = new string(word);
+                    // Console.WriteLine(test);
                 }
             }
         }
+        public char[] Switch(char[] word) {
+            var last = word.Length - 1;
+            char temp = word[last];
+            word[last] = word[last - 1];
+            word[last - 1] = temp;
+            return word;
+        }
+        public char[] ShiftToRight(char[] word)
+        {
+            var last = word.Length - 1;
+            char temp = word[last];
+            for (int i = word.Length - 1; i > 0; i--) {
+                word[i] = word[i - 1];
+            }
+            word[0] = temp;
+            return word;
+        }
+
     }
 
 }
