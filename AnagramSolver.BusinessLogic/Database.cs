81cf7b9eb10b2a0766348a6d23df11a4af536cfa
Tomas <kvedaravicius2@gmail.com>
2020-08-07 12:17:19 +0300

    cached word

--- AnagramSolver.BusinessLogic/Database.cs
+++ AnagramSolver.BusinessLogic/Database.cs
@@ -27,18 +27,72 @@ namespace AnagramSolver.BusinessLogic
 
             cn.Close();
         }
-        public bool CheckIfExists(string key)
+
+        public void AddToChaced(string item, int index)
         {
             SqlConnection cn = new SqlConnection();
             cn.ConnectionString = "Server=localhost;Database=anagramsolver; User Id = sa; Password = LAMA55lama;";
             cn.Open();
 
-            SqlCommand checkWord = new SqlCommand("SELECT COUNT(*) FROM [Word] WHERE ([word] = @word)");
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = cn;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "INSERT INTO CachedWord (searchWord, anagramId) " + "VALUES ('" + item.Replace("'", "''") + "', '" + index + "')";
+            cmd.ExecuteNonQuery();
+
+            cn.Close();
+        }
+        public int CheckIfExistsInWords(string key)
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = "Server=localhost;Database=anagramsolver; User Id = sa; Password = LAMA55lama;";
+            cn.Open();
+
+            SqlCommand checkWord = new SqlCommand("SELECT * FROM Word WHERE word LIKE @word");
+            checkWord.Connection = cn;
+            checkWord.Parameters.Add(new SqlParameter("word", key));
+            SqlDataReader reader = checkWord.ExecuteReader();
+            var index = 0;
+            while (reader.Read())
+            {
+               index = reader.GetInt32(0);
+            }
+            cn.Close();
+            return index;
+        }
+
+        public bool CheckIfExistsInCached(string key)
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = "Server=localhost;Database=anagramsolver; User Id = sa; Password = LAMA55lama;";
+            cn.Open();
+
+            SqlCommand checkWord = new SqlCommand("SELECT COUNT(*) FROM [CachedWord] WHERE ([searchWord] = @word)");
             checkWord.Connection = cn;
             checkWord.Parameters.AddWithValue("@word", key);
             var result = (int)checkWord.ExecuteScalar();
             cn.Close();
             return result > 0;
+        }
+        public List<string> GetCachedWords(string key)
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = "Server=localhost;Database=anagramsolver; User Id = sa; Password = LAMA55lama;";
+            
+            cn.Open();
+
+            SqlCommand checkWord = new SqlCommand("SELECT word, searchWord FROM Word, CachedWord WHERE  Word.id = CachedWord.anagramId; ");
+            checkWord.Connection = cn;
+            SqlDataReader reader = checkWord.ExecuteReader();
+            var list = new List<string>();
+            while (reader.Read())
+            {
+                if(reader.GetString(1) == key) {
+                    list.Add(reader.GetString(0));
+                }
+            }
+            cn.Close();
+            return list;
         }
         public Dictionary<string, DictionaryEntry> FilterWords(string key)
         {
@@ -56,11 +110,8 @@ namespace AnagramSolver.BusinessLogic
                 DictionaryEntry theElement = new DictionaryEntry();
                 theElement.Word = reader.GetString(1);
                 theElement.Antecedent = reader.GetString(2);
-                if(!dictionary.ContainsKey(reader.GetString(1))) {
-                    dictionary.Add(key: reader.GetString(1), value: theElement);
-                }
+                dictionary.Add(key: reader.GetString(1), value: theElement);
             }
-            
             cn.Close();
             return dictionary;
         }
