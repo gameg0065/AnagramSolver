e87ec9b4a5d87aeca7a18c93c5dd514e7f72fa3a
Tomas <kvedaravicius2@gmail.com>
2020-08-10 08:33:06 +0300

    tables

---
+++ AnagramSolver.EF.DatabaseFirst/Models/CachedWord.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AnagramSolver.EF.DatabaseFirst.Models
+{
+    public partial class CachedWord
+    {
+        public string SearchWord { get; set; }
+        public int AnagramId { get; set; }
+    }
+}
---
+++ AnagramSolver.EF.DatabaseFirst/Models/Word.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AnagramSolver.EF.DatabaseFirst.Models
+{
+    public partial class Word
+    {
+        public int Id { get; set; }
+        public string Word1 { get; set; }
+        public string Category { get; set; }
+    }
+}
