e1fcbd68dbd44f722b40673c0798b9adeef61f12
Tomas <kvedaravicius2@gmail.com>
2020-08-20 10:15:49 +0300

    events

--- AnagramSolver.UI/ConsoleOutput.cs
+++ AnagramSolver.UI/ConsoleOutput.cs
@@ -68,4 +68,78 @@ namespace AnagramSolver.UI
             // ps(answ);
         }
     }
+    public class DisplayWithEvents
+    {
+        public void PrintGeneratedAnagrams(List<string> anagrams)
+        {
+            var pub = new DisplayWithEvents();
+            var sub1 = new Subscriber("printToConsole", pub);
+            var sub2 = new Subscriber("printToFile", pub);
+
+            Console.WriteLine();
+            Console.WriteLine("Generated anagrams: ");
+            foreach (var item in anagrams)
+            {
+                pub.OnRaiseCustomEvent(new CustomEventArgs("Event triggered"), item);
+            }
+            Console.WriteLine();
+
+        }
+        public event EventHandler<CustomEventArgs> RaiseCustomEvent;
+
+        // Wrap event invocations inside a protected virtual method
+        // to allow derived classes to override the event invocation behavior
+        protected virtual void OnRaiseCustomEvent(CustomEventArgs e, string item)
+        {
+            EventHandler<CustomEventArgs> raiseEvent = RaiseCustomEvent;
+
+            // Event will be null if there are no subscribers
+            if (raiseEvent != null)
+            {
+                // Format the string to send inside the CustomEventArgs parameter
+                e.Anagram = item;
+
+                // Call to raise the event.
+                raiseEvent(this, e);
+            }
+        }
+    }
+    class Subscriber
+    {
+        private readonly string _id;
+
+        public Subscriber(string id, DisplayWithEvents pub)
+        {
+            _id = id;
+            // Subscribe to the event
+            pub.RaiseCustomEvent += HandleCustomEvent;
+        }
+
+        // Define what actions to take when the event is raised.
+        void HandleCustomEvent(object sender, CustomEventArgs e)
+        {
+            if(_id == "printToConsole") {
+                Console.WriteLine(e.Anagram);
+            } else {
+                FileStream fs;
+                StreamWriter sw;
+                fs = new FileStream("../AnagramSolver.UI/files/message.txt",
+                FileMode.Append, FileAccess.Write);
+                sw = new StreamWriter(fs);
+                sw.WriteLine(e.Anagram);
+                sw.Flush();
+                sw.Close();
+                fs.Close();
+            }
+        }
+    }
+
+    public class CustomEventArgs : EventArgs
+    {
+        public CustomEventArgs(string anagram)
+        {
+            Anagram = anagram;
+        }
+        public string Anagram { get; set; }
+    }
 }
